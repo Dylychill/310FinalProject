6d4d4a20acf2666dcfa464d89d47c4fc196767d3
Dylychill <demckone@gmail.com>
2020-12-15 17:57:23 -0800

    added Spotify functionality, stretch dnd query to monsters

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This writeup is for the UW HCDE 310 Final Project, describing primarily `main.py
 ### DND 5E
 [Link](https://www.dnd5eapi.co/api/)
 
-This API has a lot of information related to Dungeons and Dragons, speficially 5th edition. There's information about spells, monsters, magical items, etc. Predominantly returns with JSON. No authorization is required to access this API.
+A simple REST API that has a lot of information related to Dungeons and Dragons, speficially 5th edition. There's information about spells, monsters, magical items, etc. Predominantly returns with JSON. No authorization is required to access this API.
 
 ### Spotify API
 [Link](https://developer.spotify.com/documentation/web-api/quick-start/)
 
-The [search](https://developer.spotify.com/documentation/web-api/reference/search/search/) endpoint returns songs based on a search query, providing information Spotify Catalog information on the song, artist, and album. Primarily JSON response. Requires application authorization since this app does not access user information. [Authorization](https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow) requires creating a Spotify developer account and getting client credentials. This means providing the API `/api/token` endpoint with your applications Client ID and Secret Key to get an access token.
+REST API allowing for direct interaction with a user’s Spotify account or browsing Spotify’s music libraries. The [search](https://developer.spotify.com/documentation/web-api/reference/search/search/) endpoint returns songs based on a search query, providing information Spotify Catalog information on the song, artist, and album. Primarily JSON response. Requires application authorization since this app does not access user information. [Authorization](https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow) requires creating a Spotify developer account and getting client credentials. This means providing the API `/api/token` endpoint with your applications Client ID and Secret Key to get an access token.
 
 ## What this program does
 Incorporating music into D&D enhances the experience and fun. Music can make an tense situation into a life-or-death decision, can offer insight into an NPCs motives, and set the mood alongside a DM’s (Dungeon Master) exposition. This project with take the form of an interactive webpage where users can pull up information about D&D and get song recommendations that line up with their creations. Users can request information about various spells, monsters, and skills found within 5th Edition and from that gathered information create a search query to find relevant music.
--- main.py
+++ main.py
@@ -3,65 +3,200 @@
 # Utilizing a base HTML template, this file sets the routes that allow users to input some form data
 # that is used to make an API call to DnD5e API (Dungeons and Dragons 5th Edition) and get some information about various DND elements.
 # Based on these elements, users can get song recommendations (Spotify API) and chose to save some of the queried data.
-# Builds upond previous projects in this course.
+
 
 from flask import Flask, render_template, request, session
 import urllib.parse, urllib.request, urllib.error, json
 import os
 import logging
+import base64
+from secrets import CLIENT_ID, CLIENT_SECRET # authorization for Spotify API defined in another secret file
+# the above secrets file will be ignored by git
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24) # for session storage
 
+# API URLS
+basedndurl = "https://www.dnd5eapi.co/api/" # dnd api
+base_spotsearch_url = "https://api.spotify.com/v1/search" # base url for the Spotify search endpoint
+
+# Base webpage, checks for session data to fill in save. On load
 @app.route("/")
 def main_handler():
-    app.logger.info("In MainHandler function")
-    sessdata = session.get("save-data")
-    if sessdata is not None:
-        all_saved = json.loads(sessdata)
-        return render_template("template.html", saved=all_saved)
-    else:
-        return render_template("template.html")
+    spotifyAuth() # getting authorization for spotify api
+    sessdata = getSessionSaveData()
+    # if sessdata is not None:
+        # all_saved = json.loads(sessdata)
+    return render_template("template.html", saved=sessdata)
+    # else:
+    #     return render_template("template.html")
 
-@app.route("/gresponse", methods=['GET', 'POST'])
-def spell_search_handler():
-    # check if user already has save elements
-    sessdata = session.get("save-data")
+# user wants to get song reccomendations based on a dnd element
+@app.route("/musicresp", methods=['POST'])
+def getMusic():
+    # getting music
+    app.logger.info("In Music endpoint")
+    all_saved = getSessionSaveData()
+    sessdata = session.get("music")
     if sessdata is not None:
-        all_saved = json.loads(sessdata)
+        music = json.loads(sessdata)
     else:
-        all_saved = [] # each element in this list will be JSON object per spell
+        music = []
+    song = music[int(request.form.get("savesong"))] # the unique id of the song
+    name = str(song["song-name"]) + " by "
+    for artist in song["artist-names"]:
+        name = name + artist
+    link = song["link"]
+
+    current = {"name":name, "type":"SONG", "link": link}
+    all_saved.append(current)
+    data = json.dumps(all_saved)
+    session["save-data"] = data
+    return render_template("template.html", saved=all_saved)
+
+# Handles most of the DND search functionality - searching for a new query (GET) and saving dnd elements (POST)
+@app.route("/gresponse", methods=['GET', 'POST'])
+def search_handler():
+    # check if user already has save elements
+    all_saved = getSessionSaveData()
+    currentType = session.get("current-type") # whatever the element type is, save it here for later use
+    # don't check for existence of above session type because you can't interact with that data unless the GET is made first, so it will exist
     if request.method == 'GET':
-        spell = request.args.get("spell") # gets value of this textbox
-        # app.logger.info(spell)
-        if spell: # form filled out with something
-            # make a request for the spell with this variable, then fill in template with returned info
-            resp = getSpellInfo(spell) # from API
+        searchEl = request.args.get("searchEl") # gets value of this textbox
+
+        # remove trailing and leading spaces from index
+        searchEl = searchEl.strip() # if the input is only spaces, wont request from API
+        if searchEl: # form filled out with something
+            # make a request for the element  with this variable, then fill in template with returned info
+            typeEl = request.args.get("typeOf") # which radio button selected
+            currentType = typeEl # to save this globally for use in the post request
+            session["current-type"] = typeEl #save to session data for use in saving elements
+            resp = getDNDInfo(typeEl, searchEl) # from API
             if resp:
-                return render_template("template.html", spell_info=resp, saved=all_saved)
-            else: # no info for that spell returned by API
-                return render_template("template.html", no_spells=True, saved=all_saved)
+                return render_template("template.html", searched_info=resp, saved=all_saved, typeEl=typeEl)
+            else: # no info for that element returned by API
+                return render_template("template.html", no_searched=True, saved=all_saved)
         else:
-            return render_template("template.html", no_spells=True, saved=all_saved)
+            return render_template("template.html", no_searched=True, saved=all_saved)
     elif request.method == "POST":
-        el = request.form.get("savebtn") # will be the name of the spell
+        el = request.form.get("save-or-song") # will be the name of the element
+        typeEl = request.form.get("save-options")
         if el:
-            current = {"name":el, "type":"Spell"}
-            all_saved.append(current)
-            data = json.dumps(all_saved)
-            session["save-data"] = data
-            return render_template("template.html", saved=all_saved)
+            if typeEl == "save":
+                current = {"name":el, "type":currentType.upper()}
+                all_saved.append(current)
+                data = json.dumps(all_saved)
+                session["save-data"] = data
+                return render_template("template.html", saved=all_saved)
+            elif typeEl == "song":
+                # hit up Spotify for a song based on this element
+                songs = getSongs(el)
+                if songs:
+                    return render_template("template.html", saved=all_saved, music=songs)
+                else:
+                    return render_template("template.html", saved=all_saved)
+
 
 # --------Helper Functions below, not routes ----------------
 
-# given the index for the spell to get more info - spell_index is a specific syntax for writing the spell name
+
+# given the index (unique identifier) for some typeOf dnd element (spells, monsters, etc),
+# will return information about that element by accessing the dnd API
 # Example url: https://www.dnd5eapi.co/api/spells/acid-arrow/
-def getSpellInfo(spell_index):
-    fullurl = baseurl + "spells/" + spell_index
+def getDNDInfo(typeOf, index):
+    fullurl = basedndurl + typeOf + "/" + index
     fetched = safeFetch(fullurl)
     if fetched is not None:
         return json.loads(fetched.read())
-    else:
+    else: # didn't exist in the API
         return None
+
+
+# Spotify Details - only accessing public content, Client Credentials
+# Get client credentials access token from Spotify API
+# This access can be used for the search endpoint
+# Following method taken from UW HCDE 310 and edited
+# return None on error
+def spotifyAuth():
+    """Get authorization to Spotify API"""
+    # Documentation: https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+    # the Authorization in the *header*, as a Base 64-encoded
+    # string that contains the client ID and client secret key.
+    # The field must have the format:
+    # Authorizati   on: Basic <base64 encoded client_id:client_secret>
+    # To get a bearer token to work as a "client," it also needs:
+    # grant_type = "client_credentials" as a parameter
+    try:
+        # build the header
+        authorization =  base64.standard_b64encode((CLIENT_ID + ':' + CLIENT_SECRET).encode())
+        headers = {"Authorization":"Basic " + authorization.decode()}
+        # encode the params dictionary, note it needs to be byte encoded
+        params = {"grant_type" : "client_credentials"}
+        encodedparams = urllib.parse.urlencode(params).encode()
+        # request goes to POST https://accounts.spotify.com/api/token
+        request = urllib.request.Request(
+            'https://accounts.spotify.com/api/token',
+                data=encodedparams, headers=headers)
+        resp = safeFetch(request)
+        respdata = json.load(resp)
+        accessToken = respdata['access_token'] # expires after ~60 minutes
+        session["spotify-access"] = accessToken
+        return accessToken
+    except urllib.error.URLError as e:
+        print('Error trying to retrieve data: ', e)
+        if hasattr(e, "code"):
+            print("Error Code: ", e.code)
+        elif hasattr(e, "reason"):
+            print("Failed to reach the server, Reason: ", e.reason)
+        return None
+
+# Defining a query - the name of the element input by the user will be the query used for spotify
+# Get Spotify Catalog information that match a keyword string - condenses content in a simplified version
+def getSongs(query):
+    # Search endpoint documentation - https://developer.spotify.com/documentation/web-api/reference/search/search/
+    try:
+        cleaned_query = cleanQuery(query)
+        urlwithparams = base_spotsearch_url + "?q=" + cleaned_query + "&limit=5&type=track"
+        headers = {'Authorization': 'Bearer ' + session["spotify-access"]}
+        fullurl = urllib.request.Request(url = urlwithparams, headers = headers)
+        fetched = safeFetch(fullurl)
+        if fetched is not None:
+            resp = json.loads(fetched.read())
+            music = []
+            songInfo = {}
+            uniqueID = 0
+            for song in resp["tracks"]["items"]:
+                songInfo["song-name"] = song["name"]
+                songInfo["link"] = song["external_urls"]["spotify"]
+                songInfo["album-name"] = song["album"]["name"]
+                artists = []
+                for artist in song["album"]["artists"]:
+                    artists.append(artist["name"])
+                songInfo["artist-names"] = artists
+                songInfo["unique-id"] = uniqueID
+                uniqueID = uniqueID + 1
+                music.append(songInfo)
+                songInfo = {}
+            session['music'] = json.dumps(music)
+            return music
+        else:
+            return None
+    except:
+        return None
+
+# Looks in the user's session data to get any elements that have been saved previously, and returns that as an array
+def getSessionSaveData():
+    sessdata = session.get("save-data")
+    if sessdata is not None:
+        all_saved = json.loads(sessdata)
+    else:
+        all_saved = [] # each element in this list will be JSON object per element (spell/monster/etc)
+    return all_saved
+
+# cleans up search terms in form that Spotify Search API will accept
+def cleanQuery(raw):
+    # Encode spaces with the hex code %20
+    return raw.replace(" ", "%20")
 
 
 # Helper method to make json a bit more readable, does not alter content
@@ -81,8 +216,6 @@ def safeFetch(url):
         elif hasattr(e, "reason"):
             print("Failed to reach the server, Reason: ", e.reason)
         return None
-
-baseurl = "https://www.dnd5eapi.co/api/"
 
 # run it on localhost
 if __name__ == "__main__": # if running this file directly
